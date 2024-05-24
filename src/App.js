07640249f940d0aa03ab43ca1f6cd5cfae2fe4ad
abdurrahman <arahmanmh15@gmail.com>
2024-05-24 14:47:53 +0800

    useState with previous state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import HookCounter from "./components/HookCounter";
+import HookCounter2 from "./components/HookCounter2";
 
 function App() {
   return (
     <div className="App">
+      <h3>Hook Counter 2</h3>
+      <HookCounter2 />
       <h3>Hook Counter</h3>
       <HookCounter />
       <h3>Class Counter</h3>
