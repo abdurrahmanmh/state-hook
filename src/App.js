b36fda56d0fd13c9bff87295b5714e33d48ad6e4
Ryoki Tenkai Sublimasi <106302852+abdurrahmanmh@users.noreply.github.com>
2024-05-27 14:04:48 +0800

    usesState with object (#3)

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import HookCounter from "./components/HookCounter";
 import HookCounter2 from "./components/HookCounter2";
+import HookCounter3 from "./components/HookCounter3";
 
 function App() {
   return (
     <div className="App">
+      <HookCounter3/>
       <h3>Hook Counter 2</h3>
       <HookCounter2 />
       <h3>Hook Counter</h3>
