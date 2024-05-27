b761c39d35ca3edb930fc382a9d10ad0db42a5e3
Ryoki Tenkai Sublimasi <106302852+abdurrahmanmh@users.noreply.github.com>
2024-05-27 14:14:33 +0800

    useState with array (#4)

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import ClassCounter from "./components/ClassCounter";
 import HookCounter from "./components/HookCounter";
 import HookCounter2 from "./components/HookCounter2";
 import HookCounter3 from "./components/HookCounter3";
+import HookCounter4 from "./components/HookCounter4";
 
 function App() {
   return (
     <div className="App">
+
+      
+      <h3>Hook Counter 4</h3>
+      <HookCounter4/>
+      <h3>Hook Counter 3</h3>
       <HookCounter3/>
       <h3>Hook Counter 2</h3>
       <HookCounter2 />
