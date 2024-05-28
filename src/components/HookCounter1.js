bca0b965845a3d2d0cf75f427ee19f57c35dee6e
Ryoki Tenkai Sublimasi <106302852+abdurrahmanmh@users.noreply.github.com>
2024-05-28 10:01:59 +0800

    Conditionally run effects (#6)
    * useEffect after render
    
    * Conditionally run effects

--- src/components/HookCounter1.js
+++ src/components/HookCounter1.js
@@ -3,11 +3,19 @@ import React, { useEffect, useState } from "react";
 function HookCounter1() {
   const [count, setCount] = useState(0);
 
-  useEffect(()=>{
-    document.title= `You Clicked ${count} times`
-  })
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    console.log('use Effect - Updatimg document title')
+    document.title = `You Clicked ${count} times`;
+  },[count]);
   return (
     <div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      ></input>
       <button onClick={() => setCount(count + 1)}>Clicked {count} times</button>
     </div>
   );
